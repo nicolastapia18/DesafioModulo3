c8284940d2422bb8da4827187e4a78c4c7a8b896
Nico <nicotapia02@outlook.com>
2023-01-14 00:55:53 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LogoapComponent } from './component/logoap/logoap.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { AboutComponent } from './component/about/about.component';
+import { EducationComponent } from './component/education/education.component';
+import { ExperienceComponent } from './component/experience/experience.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { ProyectsComponent } from './component/proyects/proyects.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    BannerComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ProyectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
